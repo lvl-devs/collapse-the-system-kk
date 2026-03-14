afefed1cf21935994af7b20b5fdb1a8ab8caf817
gkkconan <conan.gkk@gmail.com>
2026-03-14 09:26:25 +0100

    feat: implement skip functionality and cleanup on scene shutdown in Scene3

--- src/game/scenes/scene-3.ts
+++ src/game/scenes/scene-3.ts
@@ -6,6 +6,10 @@ export default class Scene3 extends Phaser.Scene {
     private sceneAereo!: Phaser.GameObjects.Image;
     private sceneDialog!: Phaser.GameObjects.Image;
     private rainSound!: Phaser.Sound.BaseSound;
+    private skipText?: Phaser.GameObjects.Text;
+    private transitionStarted = false;
+    private fadeTimer?: Phaser.Time.TimerEvent;
+    private endTimer?: Phaser.Time.TimerEvent;
 
     constructor(){
         super("Scene3");
@@ -23,6 +27,7 @@ export default class Scene3 extends Phaser.Scene {
 
     create(){
         LevelStorage.markLoreSceneSeen("Scene3");
+        this.transitionStarted = false;
 
         const {width,height} = this.scale;
 
@@ -39,16 +44,44 @@ export default class Scene3 extends Phaser.Scene {
         .setDisplaySize(width,height)
         .setAlpha(0);
 
+        this.skipText = this.add.text(width / 2, height - 36, "Press SPACE or click to skip", {
+            fontFamily: "Pixelify Sans",
+            fontSize: "16px",
+            color: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 4,
+        }).setOrigin(0.5);
+
+        this.input.keyboard?.on("keydown-SPACE", () => {
+            this.skipScene();
+        });
+
+        this.input.on("pointerdown", () => {
+            this.skipScene();
+        });
+
         // dopo 3 secondi parte la dissolvenza
-        this.time.delayedCall(3000, ()=>{
+        this.fadeTimer = this.time.delayedCall(3000, ()=>{
 
             this.fadeToSecond();
 
         });
 
+        this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
+            this.input.keyboard?.off("keydown-SPACE");
+            this.input.off("pointerdown");
+            this.fadeTimer?.remove();
+            this.endTimer?.remove();
+            this.tweens.killTweensOf(this.sceneAereo);
+            this.tweens.killTweensOf(this.sceneDialog);
+        });
+
     }
 
     fadeToSecond(){
+        if (this.transitionStarted) return;
+        this.transitionStarted = true;
+        this.skipText?.setVisible(false);
 
         // dissolvenza prima immagine
         this.tweens.add({
@@ -65,10 +98,29 @@ export default class Scene3 extends Phaser.Scene {
         });
 
         // dopo la dissolvenza passa alla scena 4
-        this.time.delayedCall(5000, ()=>{
-            this.rainSound.stop();
-            this.scene.start("Scene4");
+        this.endTimer = this.time.delayedCall(5000, ()=>{
+            this.startNextScene();
         });
+
+    }
+
+    private skipScene(){
+        if (this.transitionStarted) {
+            this.startNextScene();
+            return;
+        }
+
+        this.fadeTimer?.remove();
+        this.fadeToSecond();
+    }
+
+    private startNextScene(){
+        if (!this.scene.isActive()) return;
+
+        this.fadeTimer?.remove();
+        this.endTimer?.remove();
+        this.rainSound?.stop();
+        this.scene.start("Scene4");
 
     }
 
